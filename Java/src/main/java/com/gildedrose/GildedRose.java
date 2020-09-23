8809b97ebbae73582d3173b631254064b3f8f4e1
Sean Cavanaugh <cavanaughmsean@gmail.com>
2020-09-23 15:06:18 -0500

    Refactoring method down, pulling out some helper methods, and adding individual unit tests

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,10 +1,17 @@
 package com.gildedrose;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public class GildedRose {
     private static final String AGED_BRIE = "Aged Brie";
     private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    private static final List<String> ITEMS_THAT_INCREASE_OVER_TIME = asList(AGED_BRIE, BACKSTAGE_PASSES);
     private static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
     private static final String CONJURED_MANA_CAKE = "Conjured Mana Cake";
+    private static final int MAXIMUM_QUALITY = 50;
     private final Item[] items;
 
     public GildedRose(Item[] items) {
@@ -16,56 +23,45 @@ public class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!AGED_BRIE.equals(items[i].name)
-                    && !BACKSTAGE_PASSES.equals(items[i].name)) {
-                if (items[i].quality > 0) {
-                    if (!SULFURAS_HAND_OF_RAGNAROS.equals(items[i].name)) {
-                        int itemQualityDegradingAmount = !CONJURED_MANA_CAKE.equals(items[i].name) ? 1 : 2;
-                        items[i].quality = items[i].quality - itemQualityDegradingAmount;
+        for (Item item : items) {
+            if (SULFURAS_HAND_OF_RAGNAROS.equals(item.name)) {
+                continue;
+            }
+            if (ITEMS_THAT_INCREASE_OVER_TIME.contains(item.name)) {
+                updateQuality(item, 1);
+                if (BACKSTAGE_PASSES.equals(item.name) && item.sellIn < 11) {
+                    updateQuality(item, 1);
+                    if (item.sellIn < 6) {
+                        updateQuality(item, 1);
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (BACKSTAGE_PASSES.equals(items[i].name)) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+                updateQuality(item, !CONJURED_MANA_CAKE.equals(item.name) ? -1 : -2);
             }
-
-            if (!SULFURAS_HAND_OF_RAGNAROS.equals(items[i].name)) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!AGED_BRIE.equals(items[i].name)) {
-                    if (!BACKSTAGE_PASSES.equals(items[i].name)) {
-                        if (items[i].quality > 0) {
-                            if (!SULFURAS_HAND_OF_RAGNAROS.equals(items[i].name)) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = 0;
-                    }
+            item.sellIn = item.sellIn - 1;
+            if (item.sellIn < 0) {
+                if (BACKSTAGE_PASSES.equals(item.name)) {
+                    item.quality = 0;
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                    updateQuality(item, AGED_BRIE.equals(item.name) ? 1 : -1);
                 }
             }
         }
+    }
+
+    private void updateQuality(Item item, int amountToChangeQuality) {
+        if (increasingQualityAndQualityIsNotAtMaximum(item, amountToChangeQuality)) {
+            item.quality = item.quality + amountToChangeQuality;
+        } else if (decreasingQualityAndQualityIsNotZero(item, amountToChangeQuality)) {
+            item.quality = item.quality + amountToChangeQuality;
+        }
+    }
+
+    private boolean increasingQualityAndQualityIsNotAtMaximum(Item item, int amountToChangeQuality) {
+        return item.quality < MAXIMUM_QUALITY && amountToChangeQuality > 0;
+    }
+
+    private boolean decreasingQualityAndQualityIsNotZero(Item item, int amountToChangeQuality) {
+        return item.quality > 0 && amountToChangeQuality < 0;
     }
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,12 +6,109 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
+    private GildedRose gildedRose;
+
+
     @Test
-    public void foo() {
-        Item[] items = new Item[]{new Item("foo", 0, 0)};
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("foo", app.getItems()[0].name);
+    public void sulfurasDoesNotDecreaseInQualityOrSellIn() throws Exception {
+        Item[] items = new Item[]{
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80)
+        };
+        gildedRose = new GildedRose(items);
+        passNumberOfDays(2);
+        assertItem(items[0], "Sulfuras, Hand of Ragnaros", 0, 80);
+    }
+
+    @Test
+    public void commonItemsDecreaseOverTime() throws Exception {
+        Item[] items = new Item[]{
+                new Item("+5 Dexterity Vest", 5, 10)
+        };
+        gildedRose = new GildedRose(items);
+        passNumberOfDays(1);
+        assertItem(items[0], "+5 Dexterity Vest", 4, 9);
+    }
+
+    @Test
+    public void commonItemsDecreaseOverTimeAfterMultipleDays() throws Exception {
+        Item[] items = new Item[]{
+                new Item("+5 Dexterity Vest", 5, 10)
+        };
+        gildedRose = new GildedRose(items);
+        passNumberOfDays(3);
+        assertItem(items[0], "+5 Dexterity Vest", 2, 7);
+    }
+
+    @Test
+    public void itemsNeverGoBelowZero() throws Exception {
+        Item[] items = new Item[]{
+                new Item("+5 Dexterity Vest", 0, 0)
+        };
+        gildedRose = new GildedRose(items);
+        passNumberOfDays(1);
+        assertItem(items[0], "+5 Dexterity Vest", -1, 0);
+    }
+
+    @Test
+    public void agedBrieIncreasesInQuality() throws Exception {
+        Item[] items = new Item[]{
+                new Item("Aged Brie", 5, 5)
+        };
+        gildedRose = new GildedRose(items);
+        passNumberOfDays(1);
+        assertItem(items[0], "Aged Brie", 4, 6);
+    }
+
+    @Test
+    public void backstagePassesIncreaseBy2From5To10DaysOut() throws Exception {
+        Item[] items = new Item[]{
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 5)
+        };
+        gildedRose = new GildedRose(items);
+        passNumberOfDays(5);
+        assertItem(items[0], "Backstage passes to a TAFKAL80ETC concert", 5, 15);
+    }
+
+    @Test
+    public void backstagePassesIncreaseBy3From5To0DaysOut() throws Exception {
+        Item[] items = new Item[]{
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 5)
+        };
+        gildedRose = new GildedRose(items);
+        passNumberOfDays(5);
+        assertItem(items[0], "Backstage passes to a TAFKAL80ETC concert", 0, 20);
+    }
+
+    @Test
+    public void backstagePassesGoToZeroAtSellInZero() throws Exception {
+        Item[] items = new Item[]{
+                new Item("Backstage passes to a TAFKAL80ETC concert", 0, 5)
+        };
+        gildedRose = new GildedRose(items);
+        passNumberOfDays(1);
+        assertItem(items[0], "Backstage passes to a TAFKAL80ETC concert", -1, 0);
+    }
+
+    @Test
+    public void conjuredItemsDecreaseTwiceAsFast() throws Exception {
+        Item[] items = new Item[]{
+                new Item("Conjured Mana Cake", 10, 10)
+        };
+        gildedRose = new GildedRose(items);
+        passNumberOfDays(3);
+        assertItem(items[0], "Conjured Mana Cake", 7, 4);
+    }
+
+    private void assertItem(Item item, String name, int sellIn, int quality) {
+        assertEquals(name, item.name);
+        assertEquals(sellIn, item.sellIn);
+        assertEquals(quality, item.quality);
+    }
+
+    private void passNumberOfDays(int days) {
+        for (int i = 0; i < days; i++) {
+            gildedRose.updateQuality();
+        }
     }
 
 }
